36f040ca9c099b7f694e0c88aa0689f5d326ed69
Yuzili <395345203@qq.com>
2017-11-20 22:09:04 +0800

    modify-second

---
+++ src/components/info/info.vue
@@ -0,0 +1,67 @@
+
+<template>
+	<div class='info-wrap'>
+		<div>
+			 <el-menu
+			  style='width:240px;'
+			  router
+			  :default-openeds=arr
+		      default-active="2"
+		      class="el-menu-vertical-demo"
+		      @open="handleOpen"
+		      @close="handleClose">
+		      <el-submenu index="1">
+		        <template slot="title">
+		          <i class="el-icon-location"></i>
+		          <span>信息管理</span>
+		        </template>
+		        <el-menu-item-group>
+		          <el-menu-item index="/info/counter">新增电影</el-menu-item>
+		          <el-menu-item index="/info/todoList">电影列表</el-menu-item>
+		          <el-menu-item index="/info/empList">新增影院</el-menu-item>
+		          <el-menu-item index="/info/empList_ant">影院列表</el-menu-item>
+		          <el-menu-item index="/info/addEmp">新增排片</el-menu-item>
+		          <el-menu-item index="/info/imgUpLoad">排片列表</el-menu-item>
+		        </el-menu-item-group>
+		        
+		      </el-submenu>
+		      <el-menu-item index="2">
+		        <i class="el-icon-menu"></i>
+		        <span slot="title">导航二</span>
+		      </el-menu-item>
+		     
+		    </el-menu>
+
+		</div>
+		<router-view></router-view>
+
+	</div>
+</template>
+
+<script>
+
+	import router from "../../router/router"
+
+	export default{
+		name:"info",
+		data(){
+			return {
+				arr:['1']
+			}
+		},
+		methods: {
+	      handleOpen(key, keyPath) {
+	        console.log(key, keyPath);
+	      },
+	      handleClose(key, keyPath) {
+	        console.log(key, keyPath);
+	      }
+	    }
+	}
+</script>
+
+<style>
+	.info-wrap{
+		display: flex;
+	}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+import app from './App'
+import router from './router/router.js'
+
+
+
+
+
+
+// element ui引入
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
+
+
+
+Vue.component('app',app)
 
 Vue.config.productionTip = false
 
@@ -10,6 +24,5 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  template: '<App/>',
-  components: { App }
+  template: '<app />',
 })
