c826a07978b83019de35018e6361931f4c5d83cb
Yuzili <395345203@qq.com>
2017-11-21 10:02:22 +0800

    新增模块store

---
+++ src/Module_store/store.js
@@ -0,0 +1,14 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex)
+
+// import todoList from '../components/todoList/store.js'
+
+
+export default new Vuex.Store({
+	modules:{
+		// todoList
+	
+	}
+})
